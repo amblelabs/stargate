6d30cb9a352eb98bd4506b90b18d4fe90be67ed9
Theo <theo@git.shardmc.su>
2025-06-29 18:10:43 +0700

    ???

--- src/main/java/dev/amble/stargate/api/v2/ServerStargate.java
+++ src/main/java/dev/amble/stargate/api/v2/ServerStargate.java
@@ -1,6 +1,7 @@
 package dev.amble.stargate.api.v2;
 
 import dev.amble.lib.data.DirectedGlobalPos;
+import dev.amble.stargate.StargateMod;
 import dev.amble.stargate.api.network.ServerStargateNetwork;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.world.ServerWorld;
@@ -9,10 +10,13 @@ import net.minecraft.util.math.Direction;
 
 public class ServerStargate extends Stargate {
 
+    private boolean canSync = false;
+
     public ServerStargate(DirectedGlobalPos pos, GateKernelRegistry.KernelCreator constructor) {
         super(constructor);
 
         this.kernel.onCreate(pos);
+        this.canSync = true;
     }
 
     public ServerStargate(NbtCompound nbt) {
@@ -21,6 +25,10 @@ public class ServerStargate extends Stargate {
 
     @Override
     public void sync() {
+        if (!this.canSync) {
+            StargateMod.LOGGER.warn("Tried to sync a stargate that was not ready yet!");
+            return;
+        }
         ServerStargateNetwork.get().syncPartial(this);
     }
 }
