b3df4631f51272097e643e573d3035a14eeeb7f5
Loqor <isaiahcrace@gmail.com>
2026-05-03 15:25:17 -0500

    add other constructor, theo please fix

--- common/src/main/java/dev/amblelabs/stargate/common/blocks/StargateBlockEntity.java
+++ common/src/main/java/dev/amblelabs/stargate/common/blocks/StargateBlockEntity.java
@@ -1,12 +1,18 @@
 package dev.amblelabs.stargate.common.blocks;
 
+import dev.amblelabs.stargate.common.lib.StargateBlockEntities;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class StargateBlockEntity extends BlockEntity {
-    public StargateBlockEntity(BlockEntityType<?> blockEntityType, BlockPos blockPos, BlockState blockState) {
-        super(blockEntityType, blockPos, blockState);
+
+    public StargateBlockEntity(BlockEntityType<?> type, BlockPos blockPos, BlockState blockState) {
+        super(type, blockPos, blockState);
+    }
+
+    public StargateBlockEntity(BlockPos blockPos, BlockState blockState) {
+        super(StargateBlockEntities.STARGATE, blockPos, blockState);
     }
 }
