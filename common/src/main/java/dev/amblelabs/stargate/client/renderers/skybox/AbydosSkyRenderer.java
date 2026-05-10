77f622ba2c93f8d34859f69a2449e4633694f07e
Addi3 <addiec787@gmail.com>
2026-05-11 00:08:28 +0100

    chore: clean up sky rendering stuff

--- common/src/main/java/dev/amblelabs/stargate/client/renderers/skybox/AbydosSkyRenderer.java
+++ common/src/main/java/dev/amblelabs/stargate/client/renderers/skybox/AbydosSkyRenderer.java
@@ -28,7 +28,11 @@ public class AbydosSkyRenderer implements IClientXplatAbstractions.SkyRenderer {
     private static final ResourceLocation MOON_LOCATION = ResourceLocation.withDefaultNamespace("textures/environment/moon_phases.png");
     private static final ResourceLocation SUN_LOCATION = ResourceLocation.withDefaultNamespace("textures/environment/sun.png");
 
-    Minecraft minecraft = Minecraft.getInstance();
+    private static final float[] MOON_X_OFFSETS = {-0, 25.0f, -28.0f};
+    private static final float[] MOON_Y_OFFSETS = {-0, -10.0f, -12.5f};
+    private static final float[] MOON_SCALES = {1, 0.5f, 0.75f};
+
+    private final Minecraft minecraft = Minecraft.getInstance();
     @Nullable
     private VertexBuffer starBuffer;
     @Nullable
@@ -71,7 +75,6 @@ public class AbydosSkyRenderer implements IClientXplatAbstractions.SkyRenderer {
 
     private static MeshData buildSkyDisc(Tesselator tesselator, float y) {
         float f = Math.signum(y) * 512.0F;
-        float g = 512.0F;
         BufferBuilder bufferBuilder = tesselator.begin(VertexFormat.Mode.TRIANGLE_FAN, DefaultVertexFormat.POSITION);
         bufferBuilder.addVertex(0.0F, y, 0.0F);
 
@@ -84,8 +87,6 @@ public class AbydosSkyRenderer implements IClientXplatAbstractions.SkyRenderer {
 
     private MeshData drawStars(Tesselator tesselator) {
         RandomSource randomSource = RandomSource.create(10842L);
-        int i = 1500;
-        float f = 100.0F;
         BufferBuilder bufferBuilder = tesselator.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION);
 
         for(int j = 0; j < 1500; ++j) {
@@ -112,46 +113,39 @@ public class AbydosSkyRenderer implements IClientXplatAbstractions.SkyRenderer {
         if (starBuffer == null) createStars();
         if (skyBuffer == null) createLightSky();
         if (darkBuffer == null) createDarkSky();
-        if (!this.minecraft.level.effects().isFoggyAt(Mth.floor(camera.getPosition().x), Mth.floor(camera.getPosition().y)) || this.minecraft.gui.getBossOverlay().shouldCreateWorldFog()) {
+        if (!level.effects().isFoggyAt(Mth.floor(camera.getPosition().x), Mth.floor(camera.getPosition().y)) || this.minecraft.gui.getBossOverlay().shouldCreateWorldFog()) {
             FogType fogType = camera.getFluidInCamera();
             if (fogType != FogType.POWDER_SNOW && fogType != FogType.LAVA && !this.doesMobEffectBlockSky(camera)) {
                 PoseStack poseStack = new PoseStack();
                 poseStack.mulPose(frustumMatrix);
-                Vec3 vec3 = minecraft.level.getSkyColor(this.minecraft.gameRenderer.getMainCamera().getPosition(), partialTick);
-                float f = (float)vec3.x;
-                float g = (float)vec3.y;
-                float h = (float)vec3.z;
                 FogRenderer.levelFogColor();
                 Tesselator tesselator = Tesselator.getInstance();
                 RenderSystem.depthMask(false);
-                RenderSystem.setShaderColor(f, g, h, 1.0F);
+                Vec3 skyColor = level.getSkyColor(this.minecraft.gameRenderer.getMainCamera().getPosition(), partialTick);
+                RenderSystem.setShaderColor((float) skyColor.x, (float) skyColor.y, (float) skyColor.z, 1.0F);
                 ShaderInstance shaderInstance = RenderSystem.getShader();
                 this.skyBuffer.bind();
                 this.skyBuffer.drawWithShader(poseStack.last().pose(), projectionMatrix, shaderInstance);
                 VertexBuffer.unbind();
                 RenderSystem.enableBlend();
-                float[] fs = minecraft.level.effects().getSunriseColor(minecraft.level.getTimeOfDay(partialTick), partialTick);
-                if (fs != null) {
+                float[] sunriseColor = level.effects().getSunriseColor(level.getTimeOfDay(partialTick), partialTick);
+                {
                     RenderSystem.setShader(GameRenderer::getPositionColorShader);
                     RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
                     poseStack.pushPose();
                     poseStack.mulPose(com.mojang.math.Axis.XP.rotationDegrees(90.0F));
-                    float i = Mth.sin(minecraft.level.getSunAngle(partialTick)) < 0.0F ? 180.0F : 0.0F;
+                    float i = Mth.sin(level.getSunAngle(partialTick)) < 0.0F ? 180.0F : 0.0F;
                     poseStack.mulPose(com.mojang.math.Axis.ZP.rotationDegrees(i));
                     poseStack.mulPose(com.mojang.math.Axis.ZP.rotationDegrees(90.0F));
-                    float j = fs[0];
-                    float k = fs[1];
-                    float l = fs[2];
                     Matrix4f matrix4f = poseStack.last().pose();
                     BufferBuilder bufferBuilder = tesselator.begin(VertexFormat.Mode.TRIANGLE_FAN, DefaultVertexFormat.POSITION_COLOR);
-                    bufferBuilder.addVertex(matrix4f, 0.0F, 100.0F, 0.0F).setColor(j, k, l, fs[3]);
-                    int m = 16;
+                    bufferBuilder.addVertex(matrix4f, 0.0F, 100.0F, 0.0F).setColor(sunriseColor[0], sunriseColor[1], sunriseColor[2], sunriseColor[3]);
 
                     for(int n = 0; n <= 16; ++n) {
                         float o = (float)n * ((float)Math.PI * 2F) / 16.0F;
                         float p = Mth.sin(o);
                         float q = Mth.cos(o);
-                        bufferBuilder.addVertex(matrix4f, p * 120.0F, q * 120.0F, -q * 40.0F * fs[3]).setColor(fs[0], fs[1], fs[2], 0.0F);
+                        bufferBuilder.addVertex(matrix4f, p * 120.0F, q * 120.0F, -q * 40.0F * sunriseColor[3]).setColor(sunriseColor[0], sunriseColor[1], sunriseColor[2], 0.0F);
                     }
 
                     BufferUploader.drawWithShader(bufferBuilder.buildOrThrow());
@@ -160,51 +154,49 @@ public class AbydosSkyRenderer implements IClientXplatAbstractions.SkyRenderer {
 
                 RenderSystem.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
                 poseStack.pushPose();
-                float i = 1.0F - minecraft.level.getRainLevel(partialTick);
-                RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, i);
+
+                float starOpacity = 1.0F - level.getRainLevel(partialTick);
+                RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, starOpacity);
                 poseStack.mulPose(com.mojang.math.Axis.YP.rotationDegrees(-90.0F));
-                poseStack.mulPose(com.mojang.math.Axis.XP.rotationDegrees(minecraft.level.getTimeOfDay(partialTick) * 360.0F));
+                poseStack.mulPose(com.mojang.math.Axis.XP.rotationDegrees(level.getTimeOfDay(partialTick) * 360.0F));
+
                 Matrix4f matrix4f2 = poseStack.last().pose();
-                float k = 30.0F;
+                float starSize = 30.0F;
                 RenderSystem.setShader(GameRenderer::getPositionTexShader);
                 RenderSystem.setShaderTexture(0, SUN_LOCATION);
-                BufferBuilder bufferBuilder2 = tesselator.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
-                bufferBuilder2.addVertex(matrix4f2, -k, 100.0F, -k).setUv(0.0F, 0.0F);
-                bufferBuilder2.addVertex(matrix4f2, k, 100.0F, -k).setUv(1.0F, 0.0F);
-                bufferBuilder2.addVertex(matrix4f2, k, 100.0F, k).setUv(1.0F, 1.0F);
-                bufferBuilder2.addVertex(matrix4f2, -k, 100.0F, k).setUv(0.0F, 1.0F);
-                BufferUploader.drawWithShader(bufferBuilder2.buildOrThrow());
-
-                float[] moonXOffsets = {-0, 25.0f, -28.0f};
-                float[] moonYOffsets = {-0, -10.0f, -12.5f};
-                float[] moonScales = {1, 0.5f, 0.75f};
+                BufferBuilder starBuffer = tesselator.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
+                starBuffer.addVertex(matrix4f2, -starSize, 100.0F, -starSize).setUv(0.0F, 0.0F);
+                starBuffer.addVertex(matrix4f2, starSize, 100.0F, -starSize).setUv(1.0F, 0.0F);
+                starBuffer.addVertex(matrix4f2, starSize, 100.0F, starSize).setUv(1.0F, 1.0F);
+                starBuffer.addVertex(matrix4f2, -starSize, 100.0F, starSize).setUv(0.0F, 1.0F);
+                BufferUploader.drawWithShader(starBuffer.buildOrThrow());
 
                 for (int j = 0; j < 3; j++) {
 
-                    float xOffset = moonXOffsets[j];
-                    float yOffset = moonYOffsets[j];
+                    float xOffset = MOON_X_OFFSETS[j];
+                    float yOffset = MOON_Y_OFFSETS[j];
 
-                    k = 20.0F * moonScales[j];
+                    starSize = 20.0F * MOON_SCALES[j];
                     RenderSystem.setShaderTexture(0, MOON_LOCATION);
-                    int r = minecraft.level.getMoonPhase();
-                    int s = r % 4;
-                    int m = r / 4 % 2;
-                    float t = (float)(s + 0) / 4.0F;
-                    float o = (float)(m + 0) / 2.0F;
+                    int moonPhase = level.getMoonPhase();
+                    int s = moonPhase % 4;
+                    int m = moonPhase / 4 % 2;
+                    float t = (float)(s) / 4.0F;
+                    float o = (float)(m) / 2.0F;
                     float p = (float)(s + 1) / 4.0F;
                     float q = (float)(m + 1) / 2.0F;
-                    bufferBuilder2 = tesselator.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
-                    bufferBuilder2.addVertex(matrix4f2, -k-xOffset, -100.0F, k-yOffset).setUv(p, q);
-                    bufferBuilder2.addVertex(matrix4f2, k-xOffset, -100.0F, k-yOffset).setUv(t, q);
-                    bufferBuilder2.addVertex(matrix4f2, k-xOffset, -100.0F, -k-yOffset).setUv(t, o);
-                    bufferBuilder2.addVertex(matrix4f2, -k-xOffset, -100.0F, -k-yOffset).setUv(p, o);
-                    BufferUploader.drawWithShader(bufferBuilder2.buildOrThrow());
+                    starBuffer = tesselator.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
+                    starBuffer.addVertex(matrix4f2, -starSize-xOffset, -100.0F, starSize-yOffset).setUv(p, q);
+                    starBuffer.addVertex(matrix4f2, starSize-xOffset, -100.0F, starSize-yOffset).setUv(t, q);
+                    starBuffer.addVertex(matrix4f2, starSize-xOffset, -100.0F, -starSize-yOffset).setUv(t, o);
+                    starBuffer.addVertex(matrix4f2, -starSize-xOffset, -100.0F, -starSize-yOffset).setUv(p, o);
+                    BufferUploader.drawWithShader(starBuffer.buildOrThrow());
                 }
 
 
-                float u = minecraft.level.getStarBrightness(partialTick) * i;
-                if (u > 0.0F) {
-                    RenderSystem.setShaderColor(u, u, u, u);
+                float starBrightness = level.getStarBrightness(partialTick) * starOpacity;
+                if (starBrightness > 0.0F) {
+                    RenderSystem.setShaderColor(starBrightness, starBrightness, starBrightness, starBrightness);
                     FogRenderer.setupNoFog();
                     this.starBuffer.bind();
                     this.starBuffer.drawWithShader(poseStack.last().pose(), projectionMatrix, GameRenderer.getPositionShader());
@@ -216,8 +208,8 @@ public class AbydosSkyRenderer implements IClientXplatAbstractions.SkyRenderer {
                 RenderSystem.defaultBlendFunc();
                 poseStack.popPose();
                 RenderSystem.setShaderColor(0.0F, 0.0F, 0.0F, 1.0F);
-                double d = this.minecraft.player.getEyePosition(partialTick).y - minecraft.level.getLevelData().getHorizonHeight(minecraft.level);
-                if (d < (double)0.0F) {
+                double d = (minecraft.player != null ? this.minecraft.player.getEyePosition(partialTick).y : 0) - level.getLevelData().getHorizonHeight(level);
+                if (d < 0.0D) {
                     poseStack.pushPose();
                     poseStack.translate(0.0F, 12.0F, 0.0F);
                     this.darkBuffer.bind();
