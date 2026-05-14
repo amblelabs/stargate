414c5be4d987368ad3df5ab90646782cdbc06ca3
Addi3 <addiec787@gmail.com>
2026-05-14 22:29:51 +0100

    fix: corrupted textures

--- fabric/src/main/java/dev/amblelabs/stargate/fabric/FabricStargateClientInit.java
+++ fabric/src/main/java/dev/amblelabs/stargate/fabric/FabricStargateClientInit.java
@@ -1,14 +1,17 @@
 package dev.amblelabs.stargate.fabric;
 
 import dev.amblelabs.stargate.client.lib.StargateClientEcs;
+import dev.amblelabs.stargate.common.lib.StargateBlocks;
 import dev.amblelabs.stargate.common.lib.StargateParticles;
 import dev.amblelabs.stargate.fabric.client.RegisterClientStuff;
 import dev.amblelabs.stargate.interop.StargateInterop;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.DimensionRenderingRegistry;
 import net.minecraft.client.particle.ParticleProvider;
 import net.minecraft.client.particle.SpriteSet;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
 import net.minecraft.client.resources.model.*;
 import net.minecraft.core.particles.ParticleOptions;
@@ -24,6 +27,10 @@ public class FabricStargateClientInit implements ClientModInitializer {
 //        HudRenderCallback.EVENT.register(AitAdditionalRenderers::overlayGui);
 
         RegisterClientStuff.init();
+        BlockRenderLayerMap.INSTANCE.putBlock(StargateBlocks.DRY_BUSH,
+                RenderType.cutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(StargateBlocks.DRY_GRASS,
+                RenderType.cutout());
 
 //        YoureFiredModelLayers.init((loc, def) -> EntityModelLayerRegistry.registerModelLayer(loc, def::get));
 
