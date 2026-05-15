52c63420a726d4b842cce61b3ecf4f59cb47cc6c
Theo <git@theo.is-a.dev>
2026-05-15 23:39:37 +0300

    fix: place gates to the opposite direction

--- common/src/main/java/dev/amblelabs/stargate/common/blocks/StargateBlock.java
+++ common/src/main/java/dev/amblelabs/stargate/common/blocks/StargateBlock.java
@@ -55,7 +55,7 @@ public class StargateBlock extends BaseEntityBlock {
 
     @Override
     public @Nullable BlockState getStateForPlacement(BlockPlaceContext context) {
-        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection());
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
     }
 
     @Override
