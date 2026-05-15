1aa34dca54037d15cce1c9996352ad21adc5613e
Theo <git@theo.is-a.dev>
2026-05-15 22:38:22 +0300

    feat: add toast and burnt toast recipes

--- common/src/main/java/dev/amblelabs/stargate/datagen/recipe/StargateXplatRecipes.java
+++ common/src/main/java/dev/amblelabs/stargate/datagen/recipe/StargateXplatRecipes.java
@@ -2,13 +2,17 @@ package dev.amblelabs.stargate.datagen.recipe;
 
 import dev.amblelabs.lib.datagen.AmbleRecipeProvider;
 import dev.amblelabs.stargate.api.StargateAPI;
+import dev.amblelabs.stargate.common.lib.StargateItems;
+import dev.amblelabs.stargate.common.recipe.ToastingRecipe;
 import dev.amblelabs.stargate.datagen.IXplatIngredients;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.*;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.ItemLike;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -26,7 +30,21 @@ public class StargateXplatRecipes extends AmbleRecipeProvider {
 
     @Override
     public void buildRecipes(RecipeOutput recipeOutput) {
+        foodToasting(recipeOutput, 10, Items.BREAD, StargateItems.TOAST, 1);
+        foodToasting(recipeOutput, 20, StargateItems.TOAST, StargateItems.BURNT_TOAST, 1);
+    }
 
+    public static void foodToasting(RecipeOutput recipeOutput, int cookingTime, ItemLike material, ItemLike result, float experience) {
+        toasting(recipeOutput, RecipeCategory.FOOD, cookingTime, material, result, experience);
+    }
+
+    public static void toasting(RecipeOutput recipeOutput, RecipeCategory category, int cookingTime, ItemLike material, ItemLike result, float experience) {
+        final String cookingMethod = "toasting";
+
+        SimpleCookingRecipeBuilder.generic(Ingredient.of(material), category, result,
+                        experience, cookingTime, ToastingRecipe.SERIALIZER, ToastingRecipe::new
+                ).unlockedBy(getHasName(material), has(material))
+                .save(recipeOutput, getItemName(result) + "_from_" + cookingMethod);
     }
 
     private void stoneCutterFromTag(RecipeOutput recipes, TagKey<Item> tagKey, Item... results) {
