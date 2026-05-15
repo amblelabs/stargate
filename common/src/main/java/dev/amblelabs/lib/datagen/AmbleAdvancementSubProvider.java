24b958232883b26e7944cf55b6a19d1d6f3870e6
Theo <git@theo.is-a.dev>
2026-05-15 22:33:11 +0300

    api(datagen): advancement builder

--- common/src/main/java/dev/amblelabs/lib/datagen/AmbleAdvancementSubProvider.java
+++ common/src/main/java/dev/amblelabs/lib/datagen/AmbleAdvancementSubProvider.java
@@ -1,14 +1,15 @@
 package dev.amblelabs.lib.datagen;
 
-import net.minecraft.advancements.AdvancementType;
-import net.minecraft.advancements.DisplayInfo;
+import net.minecraft.advancements.*;
 import net.minecraft.data.advancements.AdvancementSubProvider;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.ItemLike;
 
 import java.util.Optional;
+import java.util.function.Consumer;
 
 @SuppressWarnings({"OptionalUsedAsFieldOrParameterType", "SameParameterValue", "unused"})
 public abstract class AmbleAdvancementSubProvider implements AdvancementSubProvider {
@@ -38,5 +39,101 @@ public abstract class AmbleAdvancementSubProvider implements AdvancementSubProvi
 
     protected ResourceLocation modLoc(String name) {
         return ResourceLocation.fromNamespaceAndPath(modId, name);
+    }
+
+    public Builder create(AdvancementHolder parent, String name) {
+        return new Builder(parent, name);
+    }
+
+    public Builder create(String name) {
+        return create(null, name);
+    }
+
+    public Builder task(AdvancementHolder parent, String name) {
+        return create(parent, name);
+    }
+
+    public Builder task(String name) {
+        return create(name);
+    }
+
+    public Builder challenge(AdvancementHolder parent, String name) {
+        return create(parent, name).frame(AdvancementType.CHALLENGE);
+    }
+
+    public Builder goal(AdvancementHolder parent, String name) {
+        return create(parent, name).frame(AdvancementType.GOAL);
+    }
+
+    public class Builder {
+
+        private final Advancement.Builder builder;
+
+        private ItemLike item = Items.BARRIER;
+        private boolean hidden = false;
+        private AdvancementType frame = AdvancementType.TASK;
+        private ResourceLocation background;
+        private boolean announce = true;
+        private boolean showToast = true;
+
+        private final String name;
+
+        public Builder(AdvancementHolder parent, String name) {
+            this.builder = Advancement.Builder.advancement().parent(parent);
+            this.name = name;
+        }
+
+        public Builder condition(String name, Criterion<?> conditions) {
+            this.builder.addCriterion(name, conditions);
+            return this;
+        }
+
+        public Builder icon(ItemLike item) {
+            this.item = item;
+            return this;
+        }
+
+        public Builder hidden() {
+            this.hidden = true;
+            return this;
+        }
+
+        public Builder frame(AdvancementType frame) {
+            this.frame = frame;
+            return this;
+        }
+
+        public Builder background(ResourceLocation background) {
+            this.background = background;
+            return this;
+        }
+
+        public Builder background(String background) {
+            return background(modLoc(background));
+        }
+
+        public Builder silent() {
+            this.announce = false;
+            return this;
+        }
+
+        public Builder noToast() {
+            this.showToast = false;
+            return this;
+        }
+
+        public AdvancementHolder build(Consumer<AdvancementHolder> consumer) {
+            String modId = AmbleAdvancementSubProvider.this.modId;
+
+            AdvancementHolder holder = builder
+                    .display(item,
+                            Component.translatable("achievement." + modId + ".title." + name),
+                            Component.translatable("achievement." + modId + ".description." + name),
+                            background, frame, showToast, announce, hidden)
+                    .build(modLoc(name));
+
+            consumer.accept(holder);
+            return holder;
+        }
     }
 }
