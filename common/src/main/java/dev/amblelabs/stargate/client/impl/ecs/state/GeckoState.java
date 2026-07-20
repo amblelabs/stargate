98d242c1ca9195e265d11637e1c9fd9764c47f9a
Theo <git@theo.is-a.dev>
2026-07-21 00:19:49 +0300

    feat: toggle chevron lights

--- common/src/main/java/dev/amblelabs/stargate/client/impl/ecs/state/GeckoState.java
+++ common/src/main/java/dev/amblelabs/stargate/client/impl/ecs/state/GeckoState.java
@@ -1,18 +1,27 @@
 package dev.amblelabs.stargate.client.impl.ecs.state;
 
+import com.google.common.base.Suppliers;
 import dev.amblelabs.stargate.api.StargateAPI;
 import dev.amblelabs.stargate.api.ecs.NbtDeserializer;
 import dev.amblelabs.stargate.api.ecs.NbtSerializer;
 import dev.amblelabs.stargate.api.ecs.NbtState;
+import dev.amblelabs.stargate.api.stargate.Stargate;
 import dev.amblelabs.stargate.api.util.NbtUtil;
 import dev.amblelabs.stargate.common.blocks.StargateBlockEntity;
+import dev.amblelabs.stargate.common.impl.ecs.state.ChevronState;
+import dev.amblelabs.stargate.common.impl.ecs.state.GateState;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
+import software.bernie.geckolib.animation.AnimationState;
+import software.bernie.geckolib.cache.object.GeoBone;
 import software.bernie.geckolib.model.GeoModel;
 
+import java.util.Comparator;
+import java.util.List;
 import java.util.Objects;
+import java.util.function.Supplier;
 
 @SuppressWarnings("deprecated")
 public class GeckoState implements NbtState<GeckoState> {
@@ -53,6 +62,10 @@ public class GeckoState implements NbtState<GeckoState> {
             private final ResourceLocation texture = GeckoState.this.texture.withPath(s -> "textures/" + s + ".png");
             private final ResourceLocation animation = GeckoState.this.animation.withPath(s -> "animations/" + s + ".animation.json");
 
+            // TODO: sorting manually sucks ass.
+            private final Supplier<List<GeoBone>> lights = Suppliers.memoize(() ->
+                    this.getAnimationProcessor().getBone("lights").getChildBones().stream().sorted(Comparator.comparing(GeoBone::getName)).toList());
+
             @Override
             public ResourceLocation getModelResource(StargateBlockEntity animatable) {
                 return model;
@@ -66,6 +79,30 @@ public class GeckoState implements NbtState<GeckoState> {
             @Override
             public ResourceLocation getAnimationResource(StargateBlockEntity animatable) {
                 return animation;
+            }
+
+            // TODO: this also sucks ass
+            @Override
+            public void setCustomAnimations(StargateBlockEntity animatable, long instanceId, AnimationState<StargateBlockEntity> animationState) {
+                Stargate stargate = animatable.stargate();
+                if (stargate == null) return;
+
+                int chevrons = 0;
+                GateState.Closed closed = stargate.stateOrNull(GateState.Closed.state);
+
+                if (closed != null) {
+                    chevrons = closed.locked;
+                } else {
+                    ChevronState chevronState = stargate.stateOrNull(ChevronState.state);
+
+                    if (chevronState != null)
+                        chevrons = chevronState.chevrons;
+                }
+
+                List<GeoBone> lights = this.lights.get();
+                for (int j = 0; j < lights.size(); j++) {
+                    lights.get(j).setHidden(j >= chevrons);
+                }
             }
         });
     }
