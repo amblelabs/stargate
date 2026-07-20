08c2717d5d26fd43bc3e9423868dc69dc979159b
Theo <git@theo.is-a.dev>
2026-07-20 20:33:44 +0300

    fix: some sounds ...some sounds are gate-specific it seems, we won't handle this for now and go with milky way

--- common/src/main/java/dev/amblelabs/stargate/common/lib/StargateSounds.java
+++ common/src/main/java/dev/amblelabs/stargate/common/lib/StargateSounds.java
@@ -21,18 +21,18 @@ public class StargateSounds {
 
     public static final SoundEvent STARGATE_THEME = sound("music.theme");
 
-    public static final SoundEvent WORMHOLE_LOOP = sound("wormhole_loop");
+    public static final SoundEvent WORMHOLE_LOOP = sound("block.stargate.loop");
 
     public static final SoundEvent TOASTER_DING = sound("block.toaster.ding");
     public static final SoundEvent TOASTER_LOAD = sound("block.toaster.load");
     public static final SoundEvent TOASTER_ACTIVE = sound("block.toaster.active");
 
-    public static final SoundEvent GATE_TELEPORT = sound("gate_teleport");
-    public static final SoundEvent GATE_OPEN = sound("gate_open");
-    public static final SoundEvent GATE_CLOSE = sound("gate_close");
-    public static final SoundEvent GATE_FAIL = sound("gate_fail");
+    public static final SoundEvent GATE_TELEPORT = sound("block.stargate.tp");
+    public static final SoundEvent GATE_OPEN = sound("block.stargate.open");
+    public static final SoundEvent GATE_CLOSE = sound("block.stargate.close");
+    public static final SoundEvent GATE_FAIL = sound("block.stargate.fail");
 
-    public static final SoundEvent CHEVRON_LOCK = sound("chevron_lock");
+    public static final SoundEvent CHEVRON_LOCK = sound("block.stargate.chevron");
 
     private static SoundEvent sound(String name) {
         var id = modLoc(name);
