fb693d312dc77028fe87dd0b8539c74b27b4d331
Theo <git@theo.is-a.dev>
2026-07-23 19:59:36 +0300

    fix: register default gecko state

--- common/src/main/java/dev/amblelabs/stargate/client/lib/StargateClientEcs.java
+++ common/src/main/java/dev/amblelabs/stargate/client/lib/StargateClientEcs.java
@@ -26,6 +26,7 @@ public class StargateClientEcs {
 
     public static void initState() {
         States.register(GeckoState.state);
+        States.register(GeckoState.Default.state);
     }
 
     @SuppressWarnings("EmptyMethod")
