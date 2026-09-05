d74d09306e3cb1d7644a40176f2297e427d33256
Theo <git@theo.is-a.dev>
2026-09-05 11:28:07 +0300

    refactor: static final the fonts in gui overlay rendering

--- common/src/main/java/dev/amblelabs/stargate/client/renderers/StargateAdditionalRenderers.java
+++ common/src/main/java/dev/amblelabs/stargate/client/renderers/StargateAdditionalRenderers.java
@@ -13,18 +13,20 @@ public class StargateAdditionalRenderers {
 
     private static final Minecraft minecraft = Minecraft.getInstance();
 
+    private static final ResourceLocation FONT = ResourceLocation.fromNamespaceAndPath("amblekit", "amblestone");
+    private static final ResourceLocation FONT_SMALL = ResourceLocation.fromNamespaceAndPath("amblekit", "amblestonelite");
+
     public static void overlayGui(GuiGraphics graphics, DeltaTracker tracker) {
         if (!StargateConfig.client().renderOverlay()) return;
 
-        final Component title = Component.literal("STARGATE").withStyle(style -> style.withFont(ResourceLocation.fromNamespaceAndPath("amblekit", "amblestone")));
-        final Component edition = Component.literal(": sojourner").withStyle(style -> style.withFont(ResourceLocation.fromNamespaceAndPath("amblekit", "amblestonelite")));
+        final Component title = Component.literal("STARGATE").withStyle(style -> style.withFont(FONT));
+        final Component edition = Component.literal(": sojourner").withStyle(style -> style.withFont(FONT_SMALL));
         final Component wip = Component.literal("(work in progress)").withStyle(ChatFormatting.GRAY, ChatFormatting.ITALIC);
 
         final int padding = 2;
 
         final Font font = minecraft.font;
-        graphics.drawString(font,
-                title, padding, padding, 0xFFFFFFFF);
+        graphics.drawString(font, title, padding, padding, 0xFFFFFFFF);
 
         graphics.drawString(font, edition, padding + font.width(title) - 4, padding, 0xFFFFFF);
         graphics.drawString(font, wip, graphics.guiWidth() - font.width(wip) - padding, graphics.guiHeight() - font.lineHeight - padding, 0xFFFFFF);
