aa25d05f5093f13996c9006009b9e46e2dbca04c
Theo <git@theo.is-a.dev>
2026-09-06 20:03:29 +0300

    fix: make buried stargates more rare and not near buried treasure

--- common/src/main/java/dev/amblelabs/stargate/common/worldgen/BuriedStargateStructure.java
+++ common/src/main/java/dev/amblelabs/stargate/common/worldgen/BuriedStargateStructure.java
@@ -23,7 +23,7 @@ public class BuriedStargateStructure extends Structure {
     }
 
     private static void generatePieces(StructurePiecesBuilder builder, Structure.GenerationContext context) {
-        BlockPos blockPos = new BlockPos(context.chunkPos().getBlockX(12), 90, context.chunkPos().getBlockZ(9));
+        BlockPos blockPos = new BlockPos(context.chunkPos().getBlockX(9), 90, context.chunkPos().getBlockZ(9));
         builder.addPiece(new BuriedStargatePieces.BuriedStargatePiece(blockPos));
     }
 
